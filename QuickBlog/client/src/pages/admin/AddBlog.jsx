de8b819d0ca51fe390eee2436ea9240e3e24fef9
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-06-25 10:18:03 +0530

    addBlog.jsx file admin done

--- QuickBlog/client/src/pages/admin/AddBlog.jsx
+++ QuickBlog/client/src/pages/admin/AddBlog.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { assets } from '../../assets/assets'
+import { assets, blogCategories } from '../../assets/assets'
 import Quill from 'quill';
 
 const AddBlog = () => {
@@ -57,7 +57,23 @@ const AddBlog = () => {
           >Generate with AI</button>
         </div>
 
-        
+        <p className='mt-4'>Blog Category</p>
+        <select onChange={e => setCategory(e.target.value)} name="category" 
+        className='mt-2 px-3 py-2 border text-gray-500 border-gray-300 outline-none rounded'>
+          <option value="">Select category</option>
+          {blogCategories.map((item, index)=>{
+            return<option key={index} value={item}>{item}</option>
+          })}
+        </select>
+
+        <div className='flex gap-2 mt-4'>
+          <p>Publish Now</p>
+          <input type="checkbox"  className='scale-125 cursor-pointer'
+          onChange={e => setIsPublished(e.target.checked)}/>
+        </div>
+
+        <button type='submit' 
+        className='mt-8 w-40 h-10 bg-primary text-white rounded cursor-pointer text-sm'>Add Blog</button>
 
       </div>
     </form>

95b7f5c9f670f99ec37ab0d68e33d209c2fa417d
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-06-26 20:20:50 +0530

    search bar functions added

--- QuickBlog/client/src/components/Header.jsx
+++ QuickBlog/client/src/components/Header.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
+import { useRef } from 'react'
 
 const Header = () => {
+
+    const {setInput, input} = useAppContext()
+    const inputRef = useRef()
+
+    const onSubmitHandler = async (e)=>{
+        e.preventDefault();
+        setInput(inputRef.current.value)
+    }
+
+    const onClear = ()=>{
+        setInput('')
+        inputRef.current.value = ''
+    }
+
+
   return (
     <div className=' mx-8 sm:mx-16 xl:mx-24 relative'>
         <div className='text-center mt-20 mb-8'>
@@ -20,12 +37,21 @@ const Header = () => {
                 Whether it's one word or a thousand, your story starts right here.  
             </p>
 
-            <form className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden'>
-                <input type="text" placeholder='Search for blogs' required className='w-full pl-4 outline-none'/>
+            <form onSubmit={onSubmitHandler} className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden'>
+                <input ref={inputRef} type="text" placeholder='Search for blogs' required className='w-full pl-4 outline-none'/>
                 <button type='submit' className='bg-primary text-white px-8 py-2 m-1.5 rounded hover:scale-105 transition-all cursor-pointer'>Search</button>
             </form>
         
         </div>
+        
+        <div className='text-center'>
+            {
+            input && <button  onClick={onClear}
+            className='border font-light text-xs py-1 px-3 rounded-sm shadow-custom-sm cursor-pointer'>Clear Search</button>
+            }
+            </div>
+
+
         <img src={assets.gradientBackground} alt="" className='absolute top-0 left-0 w-full h-full object-cover -z-20 opacity-50'  />
     </div>
   )

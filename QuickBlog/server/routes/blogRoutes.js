a7baf2278c64e6c222afb86da8feea1574cfb7d1
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-06-26 16:16:32 +0530

    routes controler function set

--- QuickBlog/server/routes/blogRoutes.js
+++ QuickBlog/server/routes/blogRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { addBlog } from "../controllers/blogController.js";
+import { addBlog, addComment, deleteBlogById, getAllBlogs, getBlogById, getBlogComments, togglePublish } from "../controllers/blogController.js";
 import auth from "../middleware/auth.js";
 import upload from "../middleware/multer.js";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add", upload.single('image'), auth, addBlog)
+blogRouter.post("/add", upload.single('image'), auth, addBlog);
+blogRouter.get('/all', getAllBlogs);
+blogRouter.get('/:blogId', getBlogById);
+blogRouter.post('/delete', auth, deleteBlogById);
+blogRouter.post('/toggle-publish', auth, togglePublish);
+
+
+blogRouter.post('/add-comment', addComment);
+blogRouter.post('/Comments', getBlogComments);
 
 export default blogRouter;
